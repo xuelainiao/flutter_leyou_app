694fc2ec93744d47a56e453441313a72df08ce80
阿森 <2360072651@qq.com>
2024-04-02 12:46:18 +0800

    去除一些第三方sdkkey

--- lib/common/app_config.dart
+++ lib/common/app_config.dart
@@ -19,11 +19,6 @@ class AppConfig {
   static String ossPath =
       "https://public-1259264706.cos.ap-guangzhou.myqcloud.com/";
 
-  // /// socket 地址
-  // static String socketUrl = isProduction()
-  //     ? 'http://192.168.163.111:3000/socket'
-  //     : 'http://192.168.163.111:3000/socket';
-
   /// 请求超时时间
   static Duration timeout = const Duration(seconds: 60);
 
@@ -37,33 +32,25 @@ class AppConfig {
   static bool privacyStatementHasAgree = true;
 
   /// 百度地图 ios key
-  static const String amapIosKey = "UQk7PGiVY6fMhgQYIz5W5BtK2L2HROmE";
+  static const String amapIosKey = "";
 
   /// 百度地图 android key
-  static const String amapAndroidKey = "vpb7aVpQ2gb6trP1gxzpt9DUrgmXzXcv";
+  static const String amapAndroidKey = "";
 
   /// 高德地图 web
-  static const String amapWebkey = "VE5RJxtPuSqV9eTtId3sY9WtGxd9pjUV";
+  static const String amapWebkey = "";
 
   /// 极光推送 appkey
-  static const String jPushKey = "20552160646e55d1b65abdac";
+  static const String jPushKey = "";
 
   /// 极光推送注册id
   static String jPushId = '';
 
   /// 腾讯云 cos 秘钥id
-  static String secretId = "AKIDpaCRXSFtR0Ris12vkXGmIxX1JPK6lIj3";
+  static String secretId = "";
 
   /// 腾讯云 cos 秘钥key
-  static String secretKey = "Rlx7FEF9ulcepyAD64DUeu5Rio4R4oXJ";
-
-  //用于跳转H5链接，增加平台参数，用于屏蔽H5的字眼
-  static const String loginPath = '/user/pages/user/login?psj_platform=APP';
-
-  /// 盘世界H5
-  static String psjH5 = isProduction()
-      ? "https://psj.junwangfei.cn/h5"
-      : "https://192.168.110.152:8080/h5";
+  static String secretKey = "";
 
   /// 键盘高度
   static double keyBoardHeight = 0.0;
